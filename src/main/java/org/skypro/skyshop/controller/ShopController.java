6cc960cd59fab76004aa2e4d99ba9bc243953933
mikkimarko <margowitch123@gmail.com>
2026-06-12 20:06:17 +0200

    final commit

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -6,14 +6,6 @@ import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-
-
-
-
-
-
 import java.util.Collection;
 
 
